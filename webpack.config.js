b990511dee05388ab02a4fc3ddc0bb6b258683db
John Merritt <johnmerritt95@gmail.com>
2020-08-01 09:34:36 -0700

    code splitting to make react components folder with Form.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  entry: {
+    index: "./src/index.js",
+    form: "./src/js/components/Form.js",
+  },
   module: {
     rules: [
       {
