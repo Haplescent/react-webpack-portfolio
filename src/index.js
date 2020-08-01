6a1f314b6352ad629da507769897397edfa1b741
John Merritt <johnmerritt95@gmail.com>
2020-08-01 14:11:26 -0700

    added materalize-ui react components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import ReactDOM from "react-dom";
 import React, { Component } from "react";
-import Form from "./js/components/Form.js";
 import AppBar from "./js/components/AppBar.js";
+import Grid from "./js/components/Grid.js";
 
 const wrapper = document.getElementById("container");
 wrapper
   ? ReactDOM.render(
       <div>
         <AppBar />
-        <Form />
+        <br />
+        <Grid />
       </div>,
       wrapper
     )
