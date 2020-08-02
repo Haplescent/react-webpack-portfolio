6b87583592bddc838e866e376709c026efae8424
John Merritt <johnmerritt95@gmail.com>
2020-08-02 09:55:59 -0700

    moved pictures from unstable cdn to frontend

--- src/js/components/tileData.js
+++ src/js/components/tileData.js
@@ -1,80 +1,81 @@
+import philadelphia from "../../images/philadelphia.jpg";
+import JohnDrinkingWater from "../../images/JohnDrinkingWater.jpg";
+import NewYork from "../../images/NewYork.jpg";
+import Cancun from "../../images/Cancun.jpg";
+import CenoteDiving from "../../images/CenoteDiving.jpg";
+import Acting from "../../images/Acting.jpg";
+import MountFiji from "../../images/MountFiji.jpg";
+import profilepictureconverted from "../../images/profilepictureconverted.jpg";
+import tigersConverter from "../../images/tigersConverter.jpg";
+import BotanicalGardens from "../../images/BotanicalGardens.jpg";
+import BottomOfFuji from "../../images/BottomOfFuji.jpg";
+import Boat from "../../images/Boat.jpg";
+
 const tileData = [
   {
-    img:
-      "https://scontent-sjc3-1.xx.fbcdn.net/v/t31.0-8/23215757_10210753177614473_5648876322721358797_o.jpg?_nc_cat=100&_nc_sid=e3f864&_nc_ohc=qv9NpjEvNwkAX_JhXq7&_nc_ht=scontent-sjc3-1.xx&oh=665f400b8d961cee4c33bcedc6af6a6f&oe=5F4BC2A5",
-    title: "Breakfast",
-    author: "jill111",
+    img: philadelphia,
+    title: "Friends in Philadelphia",
+    author: "John",
     cols: 2,
     featured: true,
   },
   {
-    img:
-      "https://scontent-sjc3-1.xx.fbcdn.net/v/t31.0-8/10855107_816182591807861_4205934456343919804_o.jpg?_nc_cat=109&_nc_sid=cdbe9c&_nc_ohc=p8phyLe3IY4AX_B6AMZ&_nc_ht=scontent-sjc3-1.xx&oh=cdb20373040b0714818833f99438bf43&oe=5F4B801C",
-    title: "Tasty burger",
-    author: "director90",
+    img: JohnDrinkingWater,
+    title: "John Drinking Water",
+    author: "John",
   },
   {
-    img:
-      "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/10624640_819365451457423_5150039962349091944_n.jpg?_nc_cat=109&_nc_sid=cdbe9c&_nc_ohc=wdBv9jUA76QAX_oklSk&_nc_ht=scontent-sjc3-1.xx&oh=c5ff1552667431417b70365f559ee23c&oe=5F4B8834",
-    title: "Camera",
-    author: "Danson67",
+    img: NewYork,
+    title: "Friends in New York City",
+    author: "John",
   },
   {
-    img:
-      "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/10353699_10154384056305364_7455241874680607968_n.jpg?_nc_cat=100&_nc_sid=8bfeb9&_nc_ohc=jL2IInsVCPkAX8tijm-&_nc_ht=scontent-sjc3-1.xx&oh=b249105e219c2d8a91a70a335d03df13&oe=5F4D7B1A",
-    title: "Morning",
-    author: "fancycrave1",
+    img: Cancun,
+    title: "Friends in Cancun",
+    author: "John",
     featured: true,
   },
   {
-    img:
-      "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/10511110_829567050389895_7595926637864003900_n.jpg?_nc_cat=109&_nc_sid=cdbe9c&_nc_ohc=oZSDXPeNGl8AX-rsRme&_nc_ht=scontent-sjc3-1.xx&oh=446c781b6c4e555386117a7a8b567de2&oe=5F4DA34E",
-    title: "Hats",
-    author: "Hans",
+    img: CenoteDiving,
+    title: "Friends Going Scuba Diving",
+    author: "John",
   },
   {
-    img:
-      "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/1975045_10201782478152593_701436527_n.jpg?_nc_cat=109&_nc_sid=09cbfe&_nc_ohc=laLBosKYzYEAX9Vu7dI&_nc_ht=scontent-sjc3-1.xx&oh=2ef3fb485df8f65204c4d5a194f49450&oe=5F4A05AC",
-    title: "Honey",
-    author: "fancycravel",
+    img: Acting,
+    title: "Acting",
+    author: "John",
   },
   {
-    img:
-      "https://scontent-sjc3-1.xx.fbcdn.net/v/t31.0-8/1524150_10201300895633331_994901683_o.jpg?_nc_cat=108&_nc_sid=84a396&_nc_ohc=7JociIEpxC0AX-JKgpn&_nc_ht=scontent-sjc3-1.xx&oh=b214f60d74c274bd14b79d651500a74c&oe=5F4A8F9F",
-    title: "Vegetables",
-    author: "jill111",
+    img: MountFiji,
+    title: "On Top Of Mount Fiji",
+    author: "John",
     cols: 2,
   },
   {
-    img:
-      "https://images-ssl.gotinder.com/57be5e750bcd84321929df74/640x800_75_93f59a9e-69e8-4f20-8e1f-f153e0c2f5f2.webp",
-    title: "Water plant",
-    author: "BkrmadtyaKarki",
+    img: profilepictureconverted,
+    title: "Profile Picture",
+    author: "John",
   },
   {
-    img:
-      "https://images-ssl.gotinder.com/57be5e750bcd84321929df74/640x800_75_338694be-70e0-4533-8b18-cef840b32674.webp",
-    title: "Mushrooms",
-    author: "PublicDomainPictures",
+    img: tigersConverter,
+    title: "Acting Friends",
+    author: "John",
   },
   {
-    img:
-      "https://scontent-sjc3-1.xx.fbcdn.net/v/t1.0-9/34304766_10212205072710943_7918879714737913856_o.jpg?_nc_cat=102&_nc_sid=09cbfe&_nc_ohc=t3qYjojBTxgAX-jb9ve&_nc_ht=scontent-sjc3-1.xx&oh=88f7b0e8e9bc82324b7ada2a01d44ec6&oe=5F4AD7D0",
-    title: "Olive oil",
-    author: "congerdesign",
+    img: BotanicalGardens,
+    title: "At Botanical Gardens",
+    author: "John",
   },
   {
-    img:
-      "https://scontent-sjc3-1.xx.fbcdn.net/v/t31.0-8/1496169_10201300894633306_269573525_o.jpg?_nc_cat=106&_nc_sid=84a396&_nc_ohc=f4teDJSE10EAX_2xoPI&_nc_ht=scontent-sjc3-1.xx&oh=b545614c5fe7ae4a101873623c87ab7a&oe=5F4A4D9D",
-    title: "Sea star",
+    img: BottomOfFuji,
+    title: "Hiking up Mount Fuji",
     cols: 2,
-    author: "821292",
+    author: "John",
   },
   {
-    img:
-      "https://scontent-sjc3-1.xx.fbcdn.net/v/t31.0-8/11807718_10204912974973057_5596037796894078534_o.jpg?_nc_cat=103&_nc_sid=cdbe9c&_nc_ohc=CzEvpS8r3_sAX89spri&_nc_ht=scontent-sjc3-1.xx&oh=b0879b22b637513bde41b008e64d9261&oe=5F4B0E4C",
-    title: "Bike",
-    author: "danfador",
+    img: Boat,
+    title: "Friends in Croacia",
+    author: "John",
   },
 ];
 
